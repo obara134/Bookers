8fbe11e23696b8de511cd03e637938e4340c9452
obara134 <tyobara@icloud.com>
2021-03-10 20:15:33 +0000

    update_3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,23 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
+    @books = Book.all
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def edit
   end
+  
+  private
+  def blog_params
+    params.require(:book).permit(:title, :body)
+  end  
+  
+  
 end
