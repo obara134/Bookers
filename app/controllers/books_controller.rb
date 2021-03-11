1da843024ad671fcc484c412e0b57d24291ce671
obara134 <tyobara@icloud.com>
2021-03-11 22:24:52 +0000

    update_5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,10 +16,18 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to "/books/new"
+    redirect_to book_path(book.id)
+    
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+   
   end
   
   private
