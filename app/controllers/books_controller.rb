66d8cb5b6bda0f132ddc7cb9c4d89b983260869f
obara134 <tyobara@icloud.com>
2021-03-14 11:52:19 +0000

    update_9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
      @book = Book.find(params[:id])
      @books = Book.all
-     
   end
 
   def new
@@ -18,23 +19,16 @@ class BooksController < ApplicationController
    if @book.save
       flash[:notice] = "Book was successfully created." 
       redirect_to book_path(@book.id)
-      
    else
      @books = Book.all
-      render :new
+      render :index
    end
    
  end 
 
   def edit
     @book = Book.find(params[:id])
-    if @book.save
-      
-      flash[:notice] = "Book was successfully updated." 
-    else
-     @books = Book.all
-     render :edit
-    end
+    @books = Book.all
   end
   
   def destroy
